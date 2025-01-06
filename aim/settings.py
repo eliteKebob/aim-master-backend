064e7a0230e749cfa5b857df7c91d950b33f5163
Kaan <kaanozmendeveloper@gmail.com>
2025-01-06 23:23:18 +0300

    pg

--- aim/settings.py
+++ aim/settings.py
@@ -153,8 +153,6 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-print(STATIC_ROOT)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
